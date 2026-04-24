48a0906aca69c6d4e80bc26c616b98db9938e808
Hyunwoo Yoon <zkvp1278@gmail.com>
2026-04-24 15:26:12 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import V1 from './pages/v1'
-import V2 from './pages/v2'
-import V3 from './pages/v3'
-import V4 from './pages/v4'
+const pages = import.meta.glob('./pages/*/index.jsx', { eager: true })
 
 function App() {
-  const path = window.location.pathname
+  const match = window.location.pathname.match(/^\/(v\d+)/)
+  const version = match ? match[1] : 'v1'
+  const key = `./pages/${version}/index.jsx`
+  const Page = pages[key]?.default
 
-  if (path.startsWith('/v4')) return <V4 />
-  if (path.startsWith('/v3')) return <V3 />
-  if (path.startsWith('/v2')) return <V2 />
-  return <V1 />
+  if (!Page) return <div style={{ padding: 24 }}>페이지 없음: /{version}</div>
+  return <Page />
 }
 
 export default App
